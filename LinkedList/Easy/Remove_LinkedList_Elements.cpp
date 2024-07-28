74e4fe175fe8b2a95249d7cf072ac8ec3d0d3f8b
Vinayak <88428501+V102002@users.noreply.github.com>
2024-07-28 22:52:54 +0530

    Day7 Q3

---
+++ LinkedList/Easy/Remove_LinkedList_Elements.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(!head){
+            return nullptr;
+        }
+        ListNode* temp=new ListNode(0);
+        temp->next=head;
+        ListNode* current=temp;
+        while(current!=nullptr && current->next!=nullptr){
+            if(current->next->val==val){
+                current->next=current->next->next;
+            }else{
+                current=current->next;
+            }
+        }
+        return temp->next;
+    }
+};
