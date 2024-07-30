6e2a5ba3d1d9a98a55a473c8917afb2a237ce0c3
Vinayak <88428501+V102002@users.noreply.github.com>
2024-07-30 19:28:32 +0530

    Day 9 Q2

---
+++ LinkedList/Easy/LinkedList_Cycle.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+ 
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
