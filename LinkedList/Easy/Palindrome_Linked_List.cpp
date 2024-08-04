577d61e2446c539a35a6aa8ffa09b22d525f886a
Vinayak <88428501+V102002@users.noreply.github.com>
2024-08-04 23:20:22 +0530

    Day 14 Q1

---
+++ LinkedList/Easy/Palindrome_Linked_List.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+  struct ListNode {
+      int val;
+      ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int>arr;
+        ListNode* temp=head;
+        while(temp){
+            arr.push_back(temp->val);
+            temp=temp->next;
+        }
+        for(int i=0,j=arr.size()-1;i<j;i++,j--){
+            if(arr[i]!=arr[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
