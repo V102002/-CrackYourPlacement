0728c66105a741b738469c60c2896b2b3138a1e2
Vinayak <88428501+V102002@users.noreply.github.com>
2024-08-07 23:17:48 +0530

    Day 17 Q5

---
+++ LinkedList/Medium/Segregate_Even_Odd.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+  public:
+    Node* divide(Node* head) {
+        // code here
+        Node* temp=head;
+        vector<int>ans;
+        vector<int>num;
+        while(temp){
+            num.push_back(temp->data);
+            temp=temp->next;
+        }
+        for(int i=0;i<num.size();i++){
+            if(num[i]%2==0){
+                ans.push_back(num[i]);
+            }
+        }
+        for(int i=0;i<num.size();i++){
+            if(num[i]%2!=0){
+                ans.push_back(num[i]);
+            }
+        }
+        temp=head;
+        for(int i=0;i<ans.size();i++){
+            temp->data=ans[i];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
